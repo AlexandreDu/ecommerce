831b5ced11535f789119fde35598ffc473d83598
Alex <alexandre.dutay@outlook.fr>
2021-09-24 20:04:17 +0200

    début pour ajout du burger menu

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,21 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useRef } from 'react'
 import {connect} from 'react-redux';
 import { loadProducts } from '../actions/products/productsAction';
 const Home = (props) => {
 
   
+    const refTest = useRef(null)
 
+    const handleClick = () => {
+        const laRef = refTest.current
+        laRef.classList.add('test')
+    }
     return (
         <div>
 
+                
+  
+            
         </div>
     )
 
